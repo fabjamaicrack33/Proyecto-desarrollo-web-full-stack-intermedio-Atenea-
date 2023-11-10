b3263ffec39dea1e1286add91134a53bb3f269d5
Fabian J Vargas <121529365+fabjamaicrack33@users.noreply.github.com>
2023-11-09 22:34:35 -0500

    en proceso

--- Proyecto back-end/actividad M06 back-end/ejercicio_final.php
+++ Proyecto back-end/actividad M06 back-end/ejercicio_final.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" crossorigin="anonymous">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <title>Table db</title>
+    <title>Table db </title>
 </head>
 <body>
 <div class="wrapper">
